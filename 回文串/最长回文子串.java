9fa665b970f881c338664848982b90c524b870d8
jintao.bai <jintao,bai@mail.hypers.com>
2020-06-25 23:05:36 +0800

    add

--- 回文串/最长回文子串.java
+++ 回文串/最长回文子串.java
@@ -1,15 +1,49 @@
 package 回文串;
 
-
 /**
  * Created by jintao.bai on 2020/5/30 16:12
  * <p>
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  */
 public class 最长回文子串 {
-    //TODO asdas
+    // TODO
     public String longestPalindrome(String s) {
-        return s;
+        String res = "";
+        String tmp;
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                if (isPalindrome(tmp = s.substring(i, j), i, j) && (j - i) > res.length()) {
+                    res = tmp;
+                }
+            }
+        }
+        return res;
+    }
 
+    public boolean isPalindrome(String s, int left, int right) {
+        if (s == null || s.equals("")) {
+            return true;
+        }
+        int l = left, r = right;
+        s = s.toLowerCase();
+        while (l <= r) {
+            char cl = s.charAt(l);
+            char cr = s.charAt(r);
+            // 跳过非字母数字、空格这些
+            if (!Character.isLetterOrDigit(cl) || cl == ' ') {
+                ++l;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(cr) || cr == ' ') {
+                --r;
+                continue;
+            }
+            if (cl != cr) {
+                return false;
+            }
+            ++l;
+            --r;
+        }
+        return true;
     }
 }
