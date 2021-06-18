d83ca3ec96fdef8ab1b0b4b5457c87e1620bb17b
Xjint <is.bjtcn@gmail.com>
2021-06-18 19:48:15 +0800

    add

---
+++ 多线程/H2O.java
@@ -0,0 +1,63 @@
+package 多线程;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 生成H2O
+ * 
+ * 现在有两种线程，氧 oxygen 和氢 hydrogen，你的目标是组织这两种线程来产生水分子。
+ * 
+ * 存在一个屏障（barrier）使得每个线程必须等候直到一个完整水分子能够被产生出来。
+ * 
+ * 氢和氧线程会被分别给予 releaseHydrogen 和 releaseOxygen 方法来允许它们突破屏障。
+ * 
+ * 这些线程应该三三成组突破屏障并能立即组合产生一个水分子。
+ * 
+ * 你必须保证产生一个水分子所需线程的结合必须发生在下一个水分子产生之前。
+ * 
+ * 换句话说:
+ * 
+ * 如果一个氧线程到达屏障时没有氢线程到达，它必须等候直到两个氢线程到达。
+ * 如果一个氢线程到达屏障时没有其它线程到达，它必须等候直到一个氧线程和另一个氢线程到达。 书写满足这些限制条件的氢、氧线程同步代码。
+ * 
+ */
+public class H2O {
+
+    private Semaphore hCount = new Semaphore(2);
+    private Semaphore oCount = new Semaphore(1);
+    private CyclicBarrier cyclicBarrier;
+
+    public H2O() {
+        cyclicBarrier = new CyclicBarrier(3, () -> {
+            hCount.release(2);
+            oCount.release(1);
+        });
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        hCount.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        try {
+            cyclicBarrier.await();
+        } catch (BrokenBarrierException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oCount.acquire();
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        try {
+            cyclicBarrier.await();
+        } catch (BrokenBarrierException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
